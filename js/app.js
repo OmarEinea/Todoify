bee48b503e223a0d33260fef727c78c0fa5bea4f
Omar Einea <eineao@gmail.com>
2017-03-03 10:18:37 +0300

    Load Prayer Times From MuslimPro.com
    Also, introduce a new way of calculating time,
    and implement it in background controller

--- js/app.js
+++ js/app.js
@@ -9,26 +9,31 @@ angular.module("Todoify", ["ngMaterial", "LocalStorageModule"])
 	return localStorageService;
 })
 
-.controller("Background", function($scope, $ls, $timeout, $interval) {
-	// Time to next change (change interval)
-	var timeToChange = 60000 * 30;
-	function changeWall() {
-		$ls.set("wall", walls[Math.floor(Math.random() * walls.length)]);
-		$ls.set("wallDate", Date.now())	;
-	}
-	if(!$ls.get("wall") || Date.now() - $ls.get("wallDate") > timeToChange) {
-		changeWall();
-	}
-	$ls.bind($scope, "wall");
-	$timeout(function() {}, Date.now() - $ls.get("wallDate"));
+.factory("$calc", function() {
+    return function(time) {
+        if(typeof(time) == "string") {
+            time = time.split(":");
+            return +time[1] + time[0] * 60;
+        } else return time.getMinutes() + time.getHours() * 60;
+    };
 })
 
-.controller("Time", function($scope, $interval) {
-	// Store milliseconds of today at 10am until 11:30pm
-	var todayStart = (new Date()).setHours(10, 0, 0, 0);
-	var workHours = 36000 * 13.5;
+.controller("Background", function($scope, $ls, $interval) {
+	$ls.bind($scope, "wall");
+	var minute = 60000, changeWall = function() {
+		$scope.wall = walls[Math.floor(Math.random() * walls.length)];
+		$ls.set("wallDate", Date.now())	;
+	};
+	if(!$ls.get("wall") || Date.now() - $ls.get("wallDate") > 40 * minute)
+		changeWall();
+	$interval(changeWall, 20 * minute);
+})
+
+.controller("Time", function($scope, $calc, $interval) {
+	var startTime = $calc("10:00"); // AM
+	var workHours = $calc("13:30"); // 13.5 Hours
 	($scope.updateTime = function() {
-		$scope.percent = Math.round((Date.now() - todayStart) / workHours);
+		$scope.percent = Math.round(100 * ($calc(new Date()) - startTime) / workHours);
 	})();
 	$interval($scope.updateTime, 30000);
 })
@@ -43,4 +48,10 @@ angular.module("Todoify", ["ngMaterial", "LocalStorageModule"])
 	$scope.remove = function(index) {
 		$scope.todos.splice(index, 1)
 	};
+})
+
+.controller("Prayer", function($scope, $calc) {
+    var times = [], cells = document.getElementsByClassName("MPtimetable")[0].getElementsByTagName("td");
+    for(var i = 2; i < cells.length; i += 2)
+        times.push($calc(cells[i].innerText));
 });
